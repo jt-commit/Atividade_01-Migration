6e77a2000944eaac17aefe30cdff426e1e10b12d
jt-commit <jt@discente.ifpe.edu.br>
2026-05-20 21:55:59 -0300

    primeiro commit

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Category>
+ */
+class CategoryFactory extends Factory
+{
+    protected $model = Category::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->word,
+        ];
+    }
+}
+
---
+++ database/factories/PublisherFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Publisher;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Publisher>
+ */
+class PublisherFactory extends Factory
+{
+    protected $model = Publisher::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->unique()->company, // Gera um nome de empresa único
+            'address' => $this->faker->address,
+        ];
+    }
+
+}
